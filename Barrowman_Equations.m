83171366b1817b6747267d09847fc20bb30275d3
Bruno Sorban <brunosorban@users.noreply.github.com>
2020-09-11 15:03:54 -0300

    Lift calculation updated
    The way lift forces and moments are calculated was changed. Before, it was assumed all normal forces were concentrated in the rocket's center of pressure but this creates a distortion since the angle of attack in the nosecone is different from the angle in the fins. Now, the normal forces are calculated separately for nosecone, fins and tail.

--- Barrowman_Equations.m
+++ Barrowman_Equations.m
@@ -1,8 +1,7 @@
 %Entrada de dados
 %Parâmetros de referência e condições de contorno
 Lr = 127/1000; % comprimento de referência
-Ar = pi * (Lr^2) / 4; % área de referência
-M = 0.5; % número de mach
+Ar = pi * (Lr^2) / 4; % área de referênciap
         
 %Parâmetros da ogiva
 logiva = -558.29/1000; % comprimento da ogiva
@@ -13,7 +12,7 @@ Vb = 0.004253; %volume da ogiva
 rt = 127 / 2000; % raio na cauda
 l0 = -2.528; % comprimento do foguete (agr ta em 2406mm)
 x_cg = -1.278; % posição do centro de massa medido da ogiva
-Rs = 40/1000000; % Rugosidade RMC (em mm?)
+Rs = 40/1000000; % Rugosidade RMC
 
 %parâmetros das aletas
 Cr = 120 / 1000;
@@ -21,7 +20,7 @@ Ct = 40 / 1000;
 span = 100 / 1000; 
 pos_aletas = -2.367; % posição das aletas medido da ogiva
 N = 4; % número de aletas
-delta = pi/60; % valor de inclinação das aletas fixas
+delta = 0; % valor de inclinação das aletas fixas
 
 %Parâmetros da Cauda
 h = 60 / 1000; % comprimento da cauda
@@ -31,7 +30,6 @@ pos_tail = l0 + h; % posição da cauda medida da ogiva
 V = Vb + (2 * pi * (rt^2) * l0) + ( (h * pi / 3) * (rt^2 + rt*r2 + r2^2)); % volume pro Cmalfa
 
 %Entradas pro Simulink
-%pho = 0.752; %é a densidade do ar; rever para o apogeu
 mcarregado = 18.48;
 mdescarregado = 15.576;
 MatrizDeInercia = [0.03616 0 0; 0 8.458 0; 0 0 8.457];
@@ -45,8 +43,7 @@ MDot = readtable('Data/Comercial/cesaroni_wt_mdot.csv');
 MDot_array= table2array(MDot);
 MDot_input = MDot{:, 1};
 MDot_data = MDot{:, 2};
-%X1 = F(:,1);
-%X2 = F(:,2);
+
 CD = readtable('Data/Novo_CD.csv');
 CD_array = table2array(CD);
 CD_input = CD{:, 1};
@@ -56,7 +53,7 @@ CD_data = CD{:, 2};
 %Fórmulas de suporte às principais
 %Parâmtros relacionado às aletas
 Af = (Cr + Ct) * span / 2; % fin area
-%Aw = 2 * Af; % fin wetted area
+
 lambda = Ct / Cr; % Cr and Ct proportion
 tau = (( span + Ct ) / Ct);
 sweep = (Cr - Ct); % calculating que sweep distance
@@ -68,10 +65,7 @@ gamac = atan( (Cr - Ct) / (2 * span) ); % mid chord angle
 Lf = sqrt((Cr / 2 - Ct / 2) ^ 2 + span ^ 2); % pre calculus. No Physical meaning
 
 % auxiliar aerodinamic coefficients
-beta=sqrt( abs(1 - (M^2) ) ); % correction to compressible subsonic flow varialble (comes from Prandtl)
 Cf = 0.032 * ((Rs / Lr)^0.2);
-Cfc = Cf * (1 - (0.09 * (M^2)) );
-%q = (1/2) * pho * vo * vo % calculation for the dynamic pressure
 
 Awb = 2 * pi * r0 * l0; % body wetted area
 fb = l0 / (2 * rt); % total body fineness ratio
@@ -100,18 +94,15 @@ KRB=1+(a1/(a2-a3));
 %--------------------------------------------------------------------------------------------------------------------------------
 %as equações em si
 %derivatives para o CP
-Cnalfa0 = 2 * pi / beta;
-Cnalfa1 = ( 2 * pi * AR * (Af / Ar) ) / (2 + sqrt( 4 + ( (beta * AR / (cos(gamac)) )^2 ) ) );
 Cnalfat = ((4 * N * (span / Lr) ^ 2) / (1 + sqrt(1 + (2 * Lf / (Cr + Ct)) ^ 2))) * (1 + rt / (span + rt));
 CnTail= -2 * (1 - ((r2 / rt)^2));
 
 CnAlfaNose = 2 * pi * (r0^2) / Ar; %CnaTB = Cnalfat * KTB;
-%CnaBT = Cnalfat * KBT;
+
 Cnalfa = (CnAlfaNose + Cnalfat + CnTail); % derivativo do coeficiente de força normal total
-%centros de pressão (A fórmula 2 do Opk ta divergindo do Brwmn)
+
 XTB = pos_aletas - (sweep / 3) * ( (Cr + 2 * Ct) / (Cr + Ct) ) + (1/6) * (Cr + Ct - Cr * Ct / (Cr + Ct)); %centro de pressão das aletas
-%XBT = pos_aletas + (Cr / 4) + (( b1 * acosh(span / rt) - span + (pi * rt /2)) / ((rt / b1) * acosh(span / rt) + (span / rt) - (pi / 2))) * b2;
-%XB = logiva + (Vb / (2 * pi * r0^2) );
+
 XB = logiva - logiva/2;
 Xtail = pos_tail - (h/3) * (1 + ( (1 - r) / (1 - r^2) ) );
 %Cálculo do centro de pressão total
