afcf1d6c5785b4b471ec1366210c5c0c7347cc58
PatrickSampaioUSP <patricksampaio@usp.br>
2020-11-01 12:03:22 -0300

    Small modifications on layout and using degtoradians func in the delta attribute

--- scripts/Europa.m
+++ scripts/Europa.m
@@ -21,7 +21,7 @@ Ct = 40 / 1000;
 span = 100 / 1000; 
 pos_aletas = -2.367; % posição das aletas medido da ogiva
 N = 4; % número de aletas
-delta = 0.5 * pi / 180; % valor de inclinação das aletas fixas
+delta = degtorad(0.5); % valor de inclinação das aletas fixas
 
 %Parâmetros da Cauda
 h = 60 / 1000; % comprimento da cauda
